f3e7a8cc1096c122513bb6f56ee33cddfd705c14
Uche Offiah <uoffiah@bluetag-it.com>
2022-06-22 06:45:13 +0100

    Modification to make it the code more TypeScript

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -10,7 +10,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Set the network port.
   const port = process.env.PORT || 8082;
   
-  // Use the body parser middleware for post requests
+  // Use the body parser middleware for post requests.
   app.use(bodyParser.json());
 
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
